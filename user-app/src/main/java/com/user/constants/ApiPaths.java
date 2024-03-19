92658933409db5558eb1b710de398299d87876a7
pupov <pupov435@gmail.com>
2024-03-19 20:50:54 +0700

    Удалил всё, что связано с Authority

--- user-app/src/main/java/com/user/constants/ApiPaths.java
+++ user-app/src/main/java/com/user/constants/ApiPaths.java
@@ -9,5 +9,7 @@ public class ApiPaths {
 
     public static final String REGISTER = "/api/user/register";
 
-    public static final String AUTHORITY = "/api/authority";
+    public static final String CHECK_USER_BY_ID = "/api/user/check/{id}";
+
+    public static final String USER_BY_ID = "/api/user/{id}";
 }
