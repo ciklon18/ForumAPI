43a97313a0a6e98f2d6cd1c2cf528f5c4558f894
pupov <pupov435@gmail.com>
2024-04-08 10:27:06 +0700

    Обновил зависимости FileApplication Добавил миграцию Добавил логирование minioClient

--- file-app/src/main/java/com/file/config/MinioConfig.java
+++ file-app/src/main/java/com/file/config/MinioConfig.java
@@ -4,12 +4,14 @@ import io.minio.MinioClient;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @Getter
 @Setter
 @ToString
+@Slf4j
 @ConfigurationProperties("minio")
 public class MinioConfig {
 
@@ -18,9 +20,12 @@ public class MinioConfig {
     private String secretKey;
     private String bucket;
 
-
     @Bean
     public MinioClient minioClient() {
+        log.info(
+                "Minio config: url={}, accessKey={}, secretKey={}",
+                url, accessKey, secretKey
+        );
         return MinioClient.builder()
                 .endpoint(url)
                 .credentials(accessKey, secretKey)
--- file-app/src/main/java/com/file/core/repository/FileRepository.java
+++ file-app/src/main/java/com/file/core/repository/FileRepository.java
@@ -1,12 +1,14 @@
 package com.file.core.repository;
 
 import com.file.core.entity.File;
-import org.jetbrains.annotations.NotNull;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface FileRepository extends JpaRepository<File, UUID> {
 
     @NotNull
