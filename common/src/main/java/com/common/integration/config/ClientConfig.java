37a40778a0dc6d8124660a2f5f05804b7bd82505
pupov <pupov435@gmail.com>
2024-04-08 17:15:31 +0700

    Удалил лишние поля в ClientConfig

--- common/src/main/java/com/common/integration/config/ClientConfig.java
+++ common/src/main/java/com/common/integration/config/ClientConfig.java
@@ -3,8 +3,6 @@ package com.common.integration.config;
 import com.common.integration.utils.RetreiveMessageErrorDecoder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Client;
-import feign.codec.Decoder;
-import feign.codec.Encoder;
 import feign.codec.ErrorDecoder;
 import feign.okhttp.OkHttpClient;
 import lombok.AllArgsConstructor;
@@ -17,9 +15,6 @@ import org.springframework.context.annotation.Import;
 @AllArgsConstructor
 @Import({FeignClientsConfiguration.class})
 public class ClientConfig {
-
-    private final Encoder encoder;
-    private final Decoder decoder;
 
     @Bean
     public OkHttpClient client() {
