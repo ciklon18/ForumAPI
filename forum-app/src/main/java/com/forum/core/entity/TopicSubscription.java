b1eeee69e0399e9d48da7d9d3466d6d8e82e75a5
pupov <pupov435@gmail.com>
2024-05-10 13:44:29 +0700

    Добавил аннотацию @Table для TopicSubscription Добавил миграцию для forum

--- forum-app/src/main/java/com/forum/core/entity/TopicSubscription.java
+++ forum-app/src/main/java/com/forum/core/entity/TopicSubscription.java
@@ -1,9 +1,6 @@
 package com.forum.core.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +8,7 @@ import java.util.UUID;
 
 @Getter
 @Entity
+@Table(name = "topic_subscription")
 @NoArgsConstructor
 public class TopicSubscription {
     @Id
---
+++ forum-app/src/main/resources/db/migration/forum/V2__topic_subscribtion.sql
@@ -0,0 +1,5 @@
+CREATE TABLE topic_subscription(
+    id uuid,
+    user_id uuid,
+    topic_id uuid
+)
