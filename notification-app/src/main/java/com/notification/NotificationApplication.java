82627cd8cb61536784387af0a773b3c046c069f1
pupov <pupov435@gmail.com>
2024-05-10 13:34:21 +0700

    Удалил все, что связано с rabbitMQ Добавил кафку

--- notification-app/src/main/java/com/notification/NotificationApplication.java
+++ notification-app/src/main/java/com/notification/NotificationApplication.java
@@ -12,8 +12,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableSpringSecurity
 @EnableFeignClients
 @EnableApiExceptionHandler
-public class NotificationApp {
+public class NotificationApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NotificationApp.class, args);
+        SpringApplication.run(NotificationApplication.class, args);
     }
 }
